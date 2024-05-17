670750723e54ebe6027a757e706ec6a1a4a15099
Pop Plays <85472155+popQA17@users.noreply.github.com>
2024-05-18 07:41:35 +0800

    Changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, send_file, abort
-from flask_socketio import SocketIO, emit
+from flask import Flask, send_file, abort, request
+from flask_socketio import SocketIO, emit, join_room
 from flask_cors import CORS
 import io, base64
 from PIL import Image
@@ -14,16 +14,25 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth(port_numbers=[8224])
 drive = GoogleDrive(gauth)
 
-for files in os.listdir("tempPhotos"):
-    os.remove(f"tempPhotos/{files}")
-
 
 app = Flask(__name__)
 CORS(app)
 app.config['validLinks'] = []
 
+app.config['PASSWORD'] = "234123"
 app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+def getSortedPhotos():
+    photos = os.listdir("tempPhotos")
+    photos = [os.path.join('tempPhotos', f) for f in photos]
+    photos.sort(key=os.path.getctime)
+    fphotos = []
+    for photo in photos:
+        if "_noConsent" not in photo:
+            fphotos.insert(0, os.path.splitext(photo.replace('tempPhotos/', ""))[0])
+    return fphotos
 
 @app.route('/')
 def hello():
@@ -37,13 +46,17 @@ def handle_connect():
 def uploadPhoto(data):
     try:
         photoID = secrets.token_urlsafe(24)
+        if not data['givenConsent']:
+            photoID += "_noConsent"
         img = Image.open(io.BytesIO(base64.decodebytes(bytes(data['photo'], "utf-8"))))
         img.save(f'tempPhotos/{photoID}.png')
-        photo = drive.CreateFile({'title': photoID, "parents": [{"id": "1kGeEKc1uC2547CDLDh1fDbxP_DcvM2Dd"}]})
-        photo.SetContentFile(f'tempPhotos/{photoID}.png')
-        photo.Upload()
+        if  data['givenConsent']:
+            photo = drive.CreateFile({'title': photoID, "parents": [{"id": "1kGeEKc1uC2547CDLDh1fDbxP_DcvM2Dd"}]})
+            photo.SetContentFile(f'tempPhotos/{photoID}.png')
+            photo.Upload()
         app.config['validLinks'].append(photoID)
         emit("uploadPhoto", {'status': 'success', 'photoID': photoID})
+        emit("updateScreen", {'photos': getSortedPhotos()}, to="screenRoom")
         print("===== SUCCESS =====")
     except Exception as error:
         print("===== ERROR ===== \n")
@@ -58,7 +71,21 @@ def getPhoto(photoID):
         return abort(404)
 
     return send_file(f"tempPhotos/{photoID}.png")
-    
+
+@app.route('/getDirectPhoto/<photoID>')
+def getDirectPhoto(photoID):
+    if request.args['password'] == app.config['PASSWORD']:
+        return send_file(f"tempPhotos/{photoID}.png")
+
+@socketio.on('connectScreen')
+def connectScreen(data):
+    if data['password'] == app.config['PASSWORD']:
+        join_room("screenRoom")
+        emit("connectScreen", {'status': 'success', 'photos': getSortedPhotos()})
+    else:
+        emit("connectScreen", {'status': 'error'})
+
+
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
